c6d46f6fc34f4cd35e8fcbe5bade5c11fd346899
Xian <themacstore@The-macbook-pro.local>
2025-10-18 21:18:10 +0700

    first commit

---
+++ Script.js
@@ -0,0 +1,18 @@
+<a href="">
+  <img
+    src="https://media.istockphoto.com/id/1373017594/photo/headphones-on-the-orange-color-background.jpg?s=612x612&w=0&k=20&c=9SEBT-6kUjIBy33Ga-C9n6CQMd7FOUk3yC89mOAa0ts="
+    loading="lazy"
+    alt=""
+    className="w-full h-48 rounded-t-md"
+  />
+  <div className="flex items-center mt-2 pt-3 ml-4 mr-2">
+    <div className="ml-3">
+      <span className="block text-gray-900">f</span>
+      <span className="block text-gray-400 text-sm">24324</span>
+    </div>
+  </div>
+  <div className="pt-3 ml-4 mr-2 mb-3">
+    <h3 className="text-xl text-gray-900">dsd</h3>
+    <p className="text-gray-400 text-sm mt-1">sds</p>
+  </div>
+</a>;
